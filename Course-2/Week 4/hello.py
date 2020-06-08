11f3c53004d6413f5491aad1b1aee397ab0a70a5
Abhidip33 <66064258+Abhidip33@users.noreply.github.com>
2020-06-08 03:01:35 -0700

    Update hello.py

--- Course-2/Week 4/hello.py
+++ Course-2/Week 4/hello.py
@@ -1,14 +1,5 @@
-import os
-import sys
-# Receives a file name as a command line argument
-filename=sys.argv[1]
+#!/usr/bin/env python3
 
-# Checks whether the file name exist or not
-# When the file doesn't exist, it creates it by writing a line to it
-if not os.path.exists(filename):
-     with open(filename, "w") as f:
-         f.write("New file created\n")
-# When the file exist, our script print an error message and exits with an exit value of one
-else:
-    print("Error, the file {} already exists!".format(filename))
-    sys.exit(1)
+# Input function aloows for prompt the user for a cetain value which can be used in scripts
+name = input("Please enter your name: ")
+print("Hello, " + name)
